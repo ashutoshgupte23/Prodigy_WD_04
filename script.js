c680a15804234df4167bbbba45a1872d10ad91a2
ashutoshgupte23 <ashutoshofficial39@gmail.com>
2025-02-20 22:27:34 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-document.getElementById('downloadLink').addEventListener('click', function() {
-    
-    const link = document.createElement('a');
-    link.href = fileUrl;
-    link.download = 'Resume-ayush'; // The name of the downloaded file
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+document.addEventListener('DOMContentLoaded', () => {
+    // JavaScript code can go here for any interactivity you want to add.
+    console.log('Website loaded successfully!');
 });
